4c365e386a68a5cf03630ae53436315b6a71c0a2
Arthur <shouxing.sun@aliyun.com>
2017-12-02 07:45:15 +0800

    Rename submit to storm-submit.

--- storm/submit.sh
+++
@@ -1,9 +0,0 @@
-docker run \
-    -it \
-    -v $(pwd)/test:/topo \
-    --rm \
-    --net bshm \
-    storm \
-    storm jar /topo/storm-mongodb-examples-1.1.1.jar org.apache.storm.mongodb.topology.UpdateWordCount mongodb://mongodb.bshm:27017/test wordcount mongo-update
-    # /bin/bash
-    #storm jar /topo/storm-mongodb-examples-1.1.1.jar org.apache.storm.mongodb.topology.InsertWordCount mongodb://mongodb.bshm:27017/test wordcount mongo-insert
